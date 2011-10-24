896fff273c354c99d0c1827d53d5422a68bb9ada
Christoph Heinle <mail@christoph-heinle.de>
2011-10-24 19:28:52 +0200

    Hello World :-)

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: chrischt
+ * Date: 24.10.11
+ * Time: 19:24
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+     System.out.println("Hello World!");
+     }
+}
