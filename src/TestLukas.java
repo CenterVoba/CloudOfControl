3419df8395dbf07a2cd562ca91f8c3d4c25c0a9d
unknown <Lukas Lampe@LLampe-PC.(none)>
2011-10-25 17:14:25 +0200

    Test

---
+++ src/TestLukas.java
@@ -0,0 +1,10 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lukas Lampe
+ * Date: 25.10.11
+ * Time: 17:12
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestLukas {
+
+}
